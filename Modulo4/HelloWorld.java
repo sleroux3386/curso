99d95f28d0efaa373abf20f32b17f09aa61bf32e
sleroux <sleroux@keep.pt>
2016-05-09 00:29:45 +0100

    Added Modulo4 (HelloWorld)

---
+++ Modulo4/HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * Hello World em JAVA
+ * 
+ * @author Sébastien Leroux 
+ */
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
